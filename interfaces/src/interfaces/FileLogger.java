e6bc746c11a47aaffd80cc0b670dbe33889bc9f6
Zeynep Baydemir <zeynep_baydemir@outlook.com>
2021-05-03 05:21:21 +0300

    Fourth Lesson

---
+++ interfaces/src/interfaces/FileLogger.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class FileLogger implements Logger {
+
+	@Override
+	public void log(String message) {
+
+
+		System.out.println("Dosyaya loglandı : " + message);
+		
+	}
+
+}

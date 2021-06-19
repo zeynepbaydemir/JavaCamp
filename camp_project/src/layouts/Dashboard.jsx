00670416c65603109b6086417d3bb3ae247f7751
Zeynep Baydemir <zeynepbydemir@gmail.com>
2021-06-19 23:35:03 +0300

    Lesson 13 React Redux

--- camp_project/src/layouts/Dashboard.jsx
+++ camp_project/src/layouts/Dashboard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
-import Navi from "./Navi";
 import { Grid } from "semantic-ui-react";
 import { Route } from "react-router";
 import ProductDetail from "../pages/ProductDetail";
 import CartDetail from "../pages/CartDetail";
+import { ToastContainer } from "react-toastify";
 
 export default function Dashboard() {
   return (
     <div>
+      <ToastContainer position="bottom-right"/>
       <Grid>
         <Grid.Row>
           <Grid.Column width={4}>

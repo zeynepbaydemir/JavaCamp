92805fa5d1e92a9d5bdfebf04eb41d5b272463c8
Zeynep Baydemir <zeynepbydemir@gmail.com>
2021-06-23 23:47:47 +0300

    Lesson 14 React Forms & Clean Code

---
+++ camp_project/src/pages/ProductAdd.jsx
@@ -0,0 +1,43 @@
+import { Formik, Form ,Field,ErrorMessage} from 'formik'
+import React from 'react'
+import * as Yup from "yup"
+import { FormField,Button,Label } from 'semantic-ui-react'
+import KodlamaIoTextInput from '../utilities/customFormControls/KodlamaIoTextInput'
+
+export default function ProductAdd() {
+    const initialValues = { productName: "", unitPrice: 10 }
+
+    const schema = Yup.object({
+        productName: Yup.string().required("Ürün adı zorunlu"),
+        unitPrice: Yup.number().required("Ürün fiyatı zorunlu")
+    })
+
+    return (
+            <Formik
+                initialValues={initialValues}
+                validationSchema={schema}
+                onSubmit={(values)=>{
+
+                    console.log(values)
+                }}
+            >
+                <Form className="ui form">
+                    <KodlamaIoTextInput  name="productName" placeholder="Ürün adı"/>
+                    <KodlamaIoTextInput  name="unitPrice" placeholder="Ürün fiyatı"/>
+                    {/* <FormField>
+                     <Field name="productName" placeholder="Ürün adı"></Field>
+                     <ErrorMessage name="productName" render={error=>
+                         <Label pointing basic color="red" content={error}></Label>
+                     }></ErrorMessage>
+                    </FormField> */}
+                    {/* <FormField>
+                     <Field name="unitPrice" placeholder="Ürün fiyatı"></Field>
+                     <ErrorMessage name="uniPrice" render={error=>
+                         <Label pointing basic color="red" content={error}></Label>
+                     }></ErrorMessage>
+                    </FormField> */}
+                     <Button  color = "green" type="submit" >Ekle</Button>
+                </Form>
+            </Formik>
+    );
+}
